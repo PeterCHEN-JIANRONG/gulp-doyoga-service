374856fe5b2971b432f15a80f6ba8da61f79b7c9
peter.chen1024 <u0052017@gmail.com>
2021-09-03 19:01:22 +0800

    Update 2021-09-03T11:01:19.541Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,68 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); // swiper
+// swiper-course
+
+(function () {
+  'use strict'; // breakpoint where swiper will be destroyed
+  // and switches to a dual-column layout
+
+  var breakpoint = window.matchMedia('(max-width: 992px)'); // keep track of swiper instances to destroy later
+
+  var mySwiper; //////////////////////////////////////////////////////////////////
+
+  var breakpointChecker = function breakpointChecker() {
+    // if larger viewport and multi-row layout needed
+    if (breakpoint.matches === true) {
+      // clean up old instances and inline styles when available
+      if (mySwiper !== undefined) mySwiper.destroy(true, true); // or/and do nothing
+
+      return; // else if a small viewport and single column layout needed
+    } else if (breakpoint.matches === false) {
+      // fire small viewport version of swiper
+      return enableSwiper();
+    }
+  };
+
+  var enableSwiper = function enableSwiper() {
+    mySwiper = new Swiper('.swiper-course', {
+      slidesPerView: 1.5,
+      spaceBetween: 30
+    });
+  }; //////////////////////////////////////////////////////////////////
+  // keep an eye on viewport size changes
+
+
+  breakpoint.addListener(breakpointChecker); // kickstart
+
+  breakpointChecker();
+})();
+/* IIFE end */
+
+
+var swiper = new Swiper(".swiper-customer", {
+  slidesPerView: 1,
+  grid: {
+    rows: 3,
+    fill: 'row'
+  },
+  spaceBetween: 8,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.2,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 16
+    },
+    992: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
