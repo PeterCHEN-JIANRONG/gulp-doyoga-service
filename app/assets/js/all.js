3e72680b4e6cad14e735ad8506c66b13df274b39
peter.chen1024 <u0052017@gmail.com>
2021-09-07 21:52:23 +0800

    add /course-introduction 師資介紹樣式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -90,6 +90,31 @@ var swiper = new Swiper(".swiper-level", {
   }
 });
 
+var swiper = new Swiper(".swiper-teachers", {
+  slidesPerView: 1,
+  grid: {
+    rows: 4,
+    fill: 'row',
+  },
+  spaceBetween: 70,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.2,
+      grid: {
+        rows: 1,
+      },
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3.2,
+      grid: {
+        rows: 1,
+      },
+      spaceBetween: 30
+    },
+  }
+});
+
 // datepicker
 const elem = document.querySelector('input[name="appointment_date"]');
 const datepicker = new Datepicker(elem, {
